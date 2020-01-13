dcf0085cebc6847928ba3bfc7da14c9082673f4d
Lars Sætaberget <lars@saetaberget.no>
2020-01-13 23:28:26 +0100

    Moved some files around

--- runme.sh
+++ runme.sh
@@ -9,7 +9,7 @@ apt install ansible
 
 # Set correct interface name for i3status
 iface=$(netstat -i | egrep -v "Kernel|Iface|^lo " | cut -f 1 -d ' ')
-sed -i "s/ethernet [a-z0-9]* {/ethernet $iface {/" i3status.conf
-sed -i "s/ethernet [a-z0-9]*\"/ethernet $iface\"/" i3status.conf
+sed -i "s/ethernet [a-z0-9]* {/ethernet $iface {/" ./files/i3status.conf
+sed -i "s/ethernet [a-z0-9]*\"/ethernet $iface\"/" ./files/i3status.conf
 
-./default.yaml
+./files/default.yaml
