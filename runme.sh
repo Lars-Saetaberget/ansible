9fdbbe285b61e1c7535f12403fdbfed159653ace
Lars Sætaberget <lars@saetaberget.no>
2020-01-13 23:04:34 +0100

    Run-script added

---
+++ runme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+	echo "Please run as root or sudo"
+	exit 1
+fi
+
+apt install ansible
+
+# Set correct interface name for i3status
+iface=$(netstat -i | egrep -v "Kernel|Iface|^lo " | cut -f 1 -d ' ')
+sed -i "s/ethernet [a-z0-9]* {/ethernet $iface {/" i3status.conf
+
+./default.yaml
